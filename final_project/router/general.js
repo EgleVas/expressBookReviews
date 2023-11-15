a8dbe27d4d710d619cc97ab989d7abcffa040fd9
EgleVas <egle.vasileviciute@gmail.com>
2023-11-15 03:58:01 -0500

    completed tasks 10-14, improved general using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,16 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function getItemData(item){
+    return new Promise((resolve,reject)=>{
+        try {
+            resolve(item);
+        } catch(err) {
+            reject(err);
+        }
+    })
+}
+
 public_users.post("/register",function (req,res) {
     const username = req.body.username;
     const password = req.body.password;
@@ -22,43 +32,40 @@ public_users.post("/register",function (req,res) {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    getItemData(books).then(
+        (books) => res.send(JSON.stringify(books,null,4)),
+        (err) => console.log('Error getting book list')
+    )    
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     const book = books[isbn];
-
-    if (book) {
-        res.send(book);
-    } else {
-        res.send("Unable to find book!");
-    }
+    getItemData(book).then(
+        (book) => (book) ? res.send(book) : res.send("Unable to find book!"),
+        (err) => console.log("Error getting book data")
+    )    
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     const booksByAuthor = Object.values(books).filter((book) => book.author === author);
-
-    if (booksByAuthor.length > 0) {
-        res.send(booksByAuthor);
-    } else {
-        res.send("Unable to find book!");
-    }
+    getItemData(booksByAuthor).then(
+        (booksByAuthor) => (booksByAuthor.length > 0) ? res.send(booksByAuthor) : res.send("Unable to find book!"),
+        (err) => console.log("Error getting books by author data")
+    )
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     const booksByTitle = Object.values(books).filter((book) => book.title === title);
-
-    if (booksByTitle.length > 0) {
-        res.send(booksByTitle);
-    } else {
-        res.send("Unable to find book!");
-    }
+    getItemData(booksByTitle).then(
+        (booksByTitle) => (booksByTitle.length > 0) ? res.send(booksByTitle) : res.send("Unable to find book!"),
+        (err) => console.log("Error getting books by author data")
+    )
 });
 
 //  Get book review
